7587f3c80082456f9f1e5a47e01aa4c40f53d640
eezz <eezz10001@gmail.com>
2023-10-25 17:23:53 +0800

    update redis config

--- controllers/initRedis.go
+++ controllers/initRedis.go
@@ -13,7 +13,7 @@ var Redis = new(redis.Client)
 
 func InitRedis() {
 	Redis = redis.NewClient(&redis.Options{
-		Addr:     "116.62.161.248:6379",
+		Addr:     "27.132.46.38:6379",
 		Password: "1234567aB", // no password set
 		DB:       10,          // use default DB
 	})
